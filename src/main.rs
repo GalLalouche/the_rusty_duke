eaeec76435cb81e882137a7a3c3c04fa62529496
Gal PC <glpkmtg@gmmail.com>
2022-08-12 14:19:20 +0300

    Stacktrace on panic

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,22 @@
+#![feature(backtrace)]
 extern crate fstrings;
 
 use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::panic;
+
+use backtrace::Backtrace;
 
 use common::timer::GLOBAL_TIMERS;
-use std::collections::HashMap;
 
 mod common;
 mod game;
 mod view;
 
 fn main() -> () {
-    crate::game::ai::manual_simulator::go_main();
+    panic::set_hook(Box::new(|panic_info| {
+        println!("{:?}", Backtrace::new());
+    }));
 
-    GLOBAL_TIMERS.with(|map| {
-        let mut v: HashMap<&str, i64> = map.take().clone();
-        let mut v2: Vec<_> = v.into_iter().collect();
-        let total: f64 = v2.iter().map(|e| e.1 as f64).sum();
-        v2.sort_by_key(|e| -e.1);
-        for (k, s) in v2 {
-            println!(
-                "Total time for {} was {} ms ({:.2} %)",
-                k,
-                (s as f64) / 1000000.,
-                ((s as f64) / total) * 100.0,
-            );
-        }
-    })
+    crate::view::tui::main::go_main();
 }
