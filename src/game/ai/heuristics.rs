b7f16ec369e214289dc3f495ea2f5161ca926398
Gal (Windows) <>
2022-08-20 11:56:56 +0300

    Reduce duplication in Heuristics

--- src/game/ai/heuristics.rs
+++ src/game/ai/heuristics.rs
@@ -1,25 +1,25 @@
-use std::fmt::{Debug};
+use std::fmt::Debug;
 
 use crate::game::state::GameState;
 use crate::game::tile::Owner;
+
+fn diff<F>(owner: Owner, gs: &GameState, f: F) -> f64 where F: Fn(Owner, &GameState) -> f64 {
+    let owner_score = f(owner, &gs);
+    let other_score = f(owner.next_player(), &gs);
+    owner_score - other_score
+}
 
 pub trait Heuristic: Debug {
     fn name(&self) -> String;
     fn evaluate_for_owner(&self, o: Owner, gs: &GameState) -> f64;
     fn approx_evaluate_for_owner(&self, o: Owner, gs: &GameState) -> f64;
-    // TODO reduce duplication
     fn difference(&self, owner: Owner, gs: &GameState) -> f64 {
-        let owner_score = self.evaluate_for_owner(owner, &gs);
-        let other_score = self.evaluate_for_owner(owner.next_player(), &gs);
-        owner_score - other_score
+        diff(owner, gs, |o, g| self.evaluate_for_owner(o, g))
     }
     fn approx_difference(&self, owner: Owner, gs: &GameState) -> f64 {
-        let owner_score = self.approx_evaluate_for_owner(owner, &gs);
-        let other_score = self.approx_evaluate_for_owner(owner.next_player(), &gs);
-        owner_score - other_score
+        diff(owner, gs, |o, g| self.approx_evaluate_for_owner(o, g))
     }
 }
-
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum Heuristics {
@@ -32,7 +32,6 @@ pub enum Heuristics {
 impl Heuristic for Heuristics {
     fn name(&self) -> String { format!("{:?}", self) }
 
-    // TODO reduce duplication
     fn evaluate_for_owner(&self, o: Owner, gs: &GameState) -> f64 {
         match self {
             Heuristics::DukeMovementOptions =>
@@ -41,7 +40,7 @@ impl Heuristic for Heuristics {
                 10.0 * gs.get_tiles_for_owner(o).len() as f64,
             Heuristics::TotalMovementOptions =>
                 gs.all_valid_game_moves_for(o).count() as f64,
-            Heuristics::DiscardedUnits => gs.discard_bag_for(o).len() as f64 * -10.0,
+            Heuristics::DiscardedUnits => gs.discard_bag_for(o).len() as f64 * -15.0,
         }
     }
 
@@ -49,14 +48,7 @@ impl Heuristic for Heuristics {
         match self {
             Heuristics::DukeMovementOptions =>
                 gs.get_legal_moves_ignoring_guard(gs.duke_coordinate(o)).len() as f64,
-            Heuristics::TotalTilesOnBoard =>
-                10.0 * gs.get_tiles_for_owner(o).len() as f64,
-            Heuristics::TotalMovementOptions =>
-                gs.get_tiles_for_owner(o)
-                    .into_iter()
-                    .map(|e| e.1.get_current_side().actions().len())
-                    .sum::<usize>() as f64,
-            Heuristics::DiscardedUnits => gs.discard_bag_for(o).len() as f64 * -10.0,
+            e => e.evaluate_for_owner(o, gs),
         }
     }
 }
