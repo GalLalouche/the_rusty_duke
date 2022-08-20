4364663ca8cfbfb620ede256291c79269d0a6974
Gal PC <glpkmtg@gmmail.com>
2022-08-20 12:32:08 +0300

    Cleanup timer.rs

--- src/common/timer.rs
+++ src/common/timer.rs
@@ -1,20 +1,8 @@
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::time::Duration;
 
-thread_local!(pub static GLOBAL_TIMERS: RefCell<HashMap<&'static str, i64>> = RefCell::new(HashMap::new()));
-
-// pub fn time_it<A>(name: &'static str, f: impl FnOnce() -> A) -> A {
-//     let start = SystemTime::now();
-//     let result = f();
-//     let end = SystemTime::now();
-//     let total = end.duration_since(start).unwrap().as_nanos() as i64;
-//     GLOBAL_TIMERS.with(|map| {
-//         let mmap = &mut map.borrow_mut();
-//         let new_value = mmap.get(name).unwrap_or(&0) + total;
-//         mmap.insert(name, new_value);
-//     });
-//     result
-// }
+thread_local!(pub static GLOBAL_TIMERS: RefCell<HashMap<&'static str, u64>> = RefCell::new(HashMap::new()));
 
 #[macro_export]
 macro_rules! time_it_macro {
@@ -23,7 +11,7 @@ macro_rules! time_it_macro {
         let start = SystemTime::now();
         let result = $expr;
         let end = SystemTime::now();
-        let total = end.duration_since(start).unwrap().as_nanos() as i64;
+        let total = end.duration_since(start).unwrap().as_nanos() as u64;
         crate::common::timer::GLOBAL_TIMERS.with(|map| {
             let mmap = &mut map.borrow_mut();
             let new_value = mmap.get($name).unwrap_or(&0) + total;
@@ -33,6 +21,13 @@ macro_rules! time_it_macro {
     }}
 }
 
-pub fn get_time(name: &'static str) -> Option<i64> {
-    GLOBAL_TIMERS.with(|map| map.borrow().get(name).cloned())
+pub fn get_time(name: &str) -> Option<Duration> {
+    GLOBAL_TIMERS
+        .with(|map| map.borrow().get(name).cloned())
+        .map(|e| Duration::from_nanos(e as u64))
+}
+
+pub fn debug_time(name: &str) -> () {
+    let d = get_time(name).unwrap_or(Duration::new(0, 0));
+    println!("{} took {:?}", name, d)
 }
